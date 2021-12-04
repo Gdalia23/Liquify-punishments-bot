b5a359b4261c1a63ef5b50f8950af42376caf948
Gdalia23 <74269186+Gdalia23@users.noreply.github.com>
2021-12-04 21:32:03 +0200

    Update ready.js
    make it more clean

--- events/ready.js
+++ events/ready.js
@@ -20,36 +20,15 @@ mongoConnect();
 // Credit Event
 logger.info();
 
-const warn = {
-  name: "warn",
-  filename: "warn.js",
-  version: "1.0.0"
-}
 // warn command is loaded
-console.log(` :: ⬜️ Command ${warn.name} | Loaded version ${warn.version} from ("${warn.filename}")`)
+console.log(` :: ⬜️ Command warn | Loaded version 1.0.0 from ("warn.js")`)
 
-const mute = {
-  name: "mute",
-  filename: "mute.js",
-  version: "1.0.0"
-}
 // mute command is loaded
-console.log(` :: ⬜️ Command ${mute.name} | Loaded version ${mute.version} from ("${mute.filename}")`)
+console.log(` :: ⬜️ Command mute | Loaded version 1.0.0 from ("mute.js")`)
 
-const ban = {
-  name: "ban",
-  filename: "ban.js",
-  version: "1.0.0"
-}
 // ban command is loaded
-console.log(` :: ⬜️ Command ${ban.name} | Loaded version ${ban.version} from ("${ban.filename}")`)
-
-const punishmentlogs = {
-  name: "punishmentlogs",
-  filename: "punishment-logs.js",
-  version: "1.0.0"
-}
+console.log(` :: ⬜️ Command ban | Loaded version 1.0.0 from ("ban.js")`)
 
 // punishmentlogs command is loaded
-console.log(` :: ⬜️ Command ${punishmentlogs.name} | Loaded version ${punishmentlogs.version} from ("${punishmentlogs.filename}")`)
+console.log(` :: ⬜️ Command punishmentlogs | Loaded version 1.0.0 from ("punishment-logs.js")`)
 };
